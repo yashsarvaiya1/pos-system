b325a26d129b7548fd737574f3de990a54460448
Vivek Panchal <ivivekpanchal123@gmail.com>
2025-02-15 14:47:02 +0530

    [LEARN] nextjs: route handling in Next.js
    This commit demonstrates how to use API routes in Next.js.
    
    - Created a dummy user route with endpoints to create and get users.
    - Set up dynamic routes to get a particular user.
    
    In my case, I have used the Postman extension in VSCode, but there are
    also some alternatives like Thunder Client, REST Client, etc. for VSCode.
    
    Testing URLs:
    
    - To get all users:
      - Method: GET
      - URL: http://localhost:3000/api/users
    
    - To create a user:
      - Method: POST
      - URL: http://localhost:3000/api/users
      - Request body:
        ```json
        {
          "name": "panv"
        }
        ```
      - Response: Returns the ID and name of the created user.
    
    - To get a particular user:
      - Method: GET
      - URL: http://localhost:3000/api/users/1
      - Response: Returns the user with ID 1.

---
+++ frontend/nextjs-frontend/src/app/users/[id]/route.ts
@@ -0,0 +1,10 @@
+import { users } from "../route";
+
+export async function GET(
+  _request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = await params;
+  const user = users.find((user) => user.id === parseInt(id));
+  return Response.json(user);
+}
